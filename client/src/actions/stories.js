4eb86e1637aee4d8806cf8263cec2bb02a162cbe
Pierre Lasante <pierre.lasante@videotron.ca>
2025-01-11 11:30:34 -0500

    Refactored reducer constants

--- client/src/actions/stories.js
+++ client/src/actions/stories.js
@@ -1,9 +1,10 @@
 import * as api from "../api";
+import { FETCH_ALL_STORIES, UPDATE_STORY, DELETE_STORY, CREATE_STORY } from "../constants/actionTypes";
 
 export const getStories = () => async (dispatch) => {
   try {
     const { data } = await api.fetchStories();
-    dispatch({ type: "FETCH_ALL_STORIES", payload: data });
+    dispatch({ type: FETCH_ALL_STORIES, payload: data });
   } catch(error) {
     console.log(error.message);
   }
@@ -14,7 +15,7 @@ export const createStory = (story) => async (dispatch) => {
     // We make a post request to the backend server
     const {data} = await api.createStory(story);
     // We then update the state.stories of the store
-    dispatch({ type: "CREATE_STORY", payload: data });
+    dispatch({ type: CREATE_STORY, payload: data });
   } catch (error) {
     console.log(error.message);
   }
@@ -23,7 +24,7 @@ export const createStory = (story) => async (dispatch) => {
 export const updateStory = (id, story) => async (dispatch) => {
   try {
     const {data} = await api.updateStory(id, story);
-    dispatch({ type: "UPDATE_STORY", payload: data });
+    dispatch({ type: UPDATE_STORY, payload: data });
   } catch (error) {
     console.log(error.message);
   }
@@ -32,7 +33,7 @@ export const updateStory = (id, story) => async (dispatch) => {
 export const deleteStory = (id) => async (dispatch) => {
   try {
     await api.deleteStory(id);
-    dispatch({ type: "DELETE_STORY", payload: id });
+    dispatch({ type: DELETE_STORY, payload: id });
   } catch (error) {
     console.log(error.message);
   }
@@ -41,7 +42,7 @@ export const deleteStory = (id) => async (dispatch) => {
 export const likeStory = (id) => async (dispatch) => {
   try {
     const {data} = await api.likeStory(id);
-    dispatch({ type: "LIKE_STORY", payload: data });
+    dispatch({ type: UPDATE_STORY, payload: data });
   } catch (error) {
     console.log(error.message);
   }
