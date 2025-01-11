35212cf52049ddccf4357063bea61befb3192be7
Pierre Lasante <pierre.lasante@videotron.ca>
2025-01-11 09:37:09 -0500

    final

--- client/src/components/StoryForm/StoryForm.js
+++ client/src/components/StoryForm/StoryForm.js
@@ -1,15 +1,15 @@
-import React, { useEffect } from 'react';
-import { useDispatch } from "react-redux";
+import React, {useEffect} from 'react';
+import {useDispatch} from "react-redux";
 import styles from './styles';
 import {Button, Card, Form, Input, Typography} from "antd";
 import FileBase64 from "react-file-base64";
 import {createStory, updateStory} from "../../actions/stories";
-import { useSelector } from "react-redux";
+import {useSelector} from "react-redux";
 
 
-const { Title } = Typography;
+const {Title} = Typography;
 
-const StoryForm = ({ selectedId, setSelectedId }) => {
+const StoryForm = ({selectedId, setSelectedId}) => {
   const story = useSelector(state => selectedId ? state.stories.find(story => story._id === selectedId) : null);
 
   const dispatch = useDispatch();
@@ -34,33 +34,38 @@ const StoryForm = ({ selectedId, setSelectedId }) => {
     }
   }, [story, form]);
 
+  const reset = () => {
+    form.resetFields();
+    setSelectedId(null);
+  }
+
   return (
     <Card
       style={styles.formCard}
       title={
         <Title level={4} style={styles.formTitle}>
-          { selectedId ? "Editing a story" : "Share a story" }
+          {selectedId ? "Editing a story" : "Share a story"}
         </Title>
       }
     >
       <Form
         form={form}
-        labelCol={{ span: 6 }}
-        wrapperCol={{ span: 16 }}
+        labelCol={{span: 6}}
+        wrapperCol={{span: 16}}
         layout="horizontal"
         size="middle"
         onFinish={onSubmit}
       >
         <Form.Item label="Username" name="username" rules={[{required: true}]}>
-          <Input allowClear={true} />
+          <Input allowClear={true}/>
         </Form.Item>
         <Form.Item label="Caption" name="caption" rules={[{required: true}]}>
-          <Input.TextArea allowClear={true} autosize={{minRow: 2, maxRows: 6}} />
+          <Input.TextArea allowClear={true} autosize={{minRow: 2, maxRows: 6}}/>
         </Form.Item>
         <Form.Item label="Tags" name="tags">
-          <Input.TextArea allowClear={true} autosize={{minRow: 2, maxRows: 6}} />
+          <Input.TextArea allowClear={true} autosize={{minRow: 2, maxRows: 6}}/>
         </Form.Item>
-        <Form.Item label="Image" name="image"rules={[{required: true}]}>
+        <Form.Item label="Image" name="image" rules={[{required: true}]}>
           <FileBase64
             type={"file"}
             multiple={false}
@@ -85,6 +90,24 @@ const StoryForm = ({ selectedId, setSelectedId }) => {
             Share
           </Button>
         </Form.Item>
+        { !selectedId ? null :
+          <Form.Item
+            wrapperCol={{
+              span: 16,
+              offset: 6
+            }}
+          >
+            <Button
+              type="primary"
+              block
+              htmlType="button"
+              danger
+              onClick={reset}
+            >
+              Discard
+            </Button>
+          </Form.Item>
+        }
       </Form>
     </Card>
   );
