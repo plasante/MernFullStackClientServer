2fd34178b60e446c63649d882e15be17818c7660
Pierre Lasante <pierre.lasante@videotron.ca>
2025-01-12 07:49:59 -0500

    refactored AppBar.js

--- client/src/components/AppBar/AppBar.js
+++ client/src/components/AppBar/AppBar.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import styles from "./styles";
 import Logo from "../../images/Instaverse.png";
-import Title from "antd/es/skeleton/Title";
 import {Header} from "antd/es/layout/layout";
 import { Layout, Image, Avatar, Typography, Button } from "antd";
+
+const {Title} = Typography;
 
 const AppBar = () => {
   return (
