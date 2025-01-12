c9dde275d1d7a894f943fa04e5dff1521027a24f
Pierre Lasante <pierre.lasante@videotron.ca>
2025-01-12 09:15:46 -0500

    Implemented AppBar et Routes Route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,17 +1,24 @@
 import React from 'react';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Layout } from "antd";
 import Home from './components/Home';
 import styles from './styles';
 import AppBar from "./components/AppBar";
+import AuthForm from "./components/AuthForm";
 const {Footer } = Layout;
 
 const App = () => {
   return (
-    <Layout style={styles.layout}>
-      <AppBar/>
-      <Home />
-      <Footer  style={styles.footer}>2021 Instaverse</Footer>
-    </Layout>
+    <BrowserRouter>
+      <Layout style={styles.layout}>
+        <AppBar />
+        <Routes>
+          <Route path="/" element={<Home />}/>
+          <Route path="/authform" element={<AuthForm />}/>
+        </Routes>
+        <Footer  style={styles.footer}>2021 Instaverse</Footer>
+      </Layout>
+    </BrowserRouter>
   )
 }
 
--- client/src/components/AppBar/AppBar.js
+++ client/src/components/AppBar/AppBar.js
@@ -2,16 +2,42 @@ import React from 'react';
 import styles from "./styles";
 import Logo from "../../images/Instaverse.png";
 import {Header} from "antd/es/layout/layout";
-import { Layout, Image, Avatar, Typography, Button } from "antd";
-
+import {Avatar, Button, Image, Typography} from "antd";
+import {Link} from "react-router-dom";
 const {Title} = Typography;
 
 const AppBar = () => {
+  const user = null;
+
   return (
     <Header style={styles.header}>
-      <Image style={styles.image} preview={false} src={Logo} width={45}/>
-      &nbsp;
-      <Title style={styles.title}>Instaverse</Title>
+      <Link to="/">
+        <div style={styles.homeLink}>
+          <Image  style={styles.image} preview={false} src={Logo} width={45} />
+          &nbsp;
+          <Title  style={styles.title}>Instaverse</Title>
+        </div>
+      </Link>
+      { !user ? (
+        <Link to="authform">
+          <Button htmlType={'button'} style={styles.login}>
+            Log In
+          </Button>
+        </Link>
+      ) : (
+        <div style={styles.userInfo}>
+          <Avatar style={styles.avatar} alt={'username'} size={'large'}>
+            U
+          </Avatar>
+          <Title style={styles.title} level={4}>
+            John Doe
+          </Title>
+          <Button htmlType={'button'}>
+            Log Out
+          </Button>
+        </div>
+      )
+      }
     </Header>
   );
 };
