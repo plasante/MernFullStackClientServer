9df28fa99c1c748ee75e46c3db9cc368e947b1a2
Pierre Lasante <pierre.lasante@videotron.ca>
2025-01-18 07:48:05 -0500

    Implementing middleware authentication.js

--- server/middlewares/authentication.js
+++ server/middlewares/authentication.js
@@ -1,2 +1,25 @@
 import jwt from 'jsonwebtoken';
+import req from "express/lib/request.js";
 
+const authentication = async (req, res, next) => {
+  try {
+
+    // get the token from the frontend header
+    const token  = req.headers.authorization.split(' ')[1];
+
+    if (token) {
+      // With decodedToken we know which user is logged in
+      const decodedToken = jwt.verify(token, 'JWT_SECRET');
+      // This is how we get the userId from the decodedToken
+      req.userId = decodedToken?.id;
+    }
+
+    // When an action is triggered we check something and then we do something next.
+    next();
+
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+export default authentication;
--- server/routes/stories.js
+++ server/routes/stories.js
@@ -1,12 +1,12 @@
 import { Router } from 'express';
 import { getStories, createStory, updateStory, deleteStory, likeStory } from "../controllers/stories.js";
-
 const router = Router();
+import authentication from "../middlewares/authentication.js";
 
 router.get('/', getStories);
-router.post('/', createStory);
-router.patch('/:id', updateStory);
-router.delete('/:id', deleteStory);
-router.patch('/:id/likeStory', likeStory);
+router.post('/', authentication, createStory);
+router.patch('/:id', authentication, updateStory);
+router.delete('/:id', authentication, deleteStory);
+router.patch('/:id/likeStory', authentication, likeStory);
 
 export default router;
